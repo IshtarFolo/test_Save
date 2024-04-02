e378afe7269bb5963aca6c6ae28fc194ab071063
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-04-02 10:55:57 -0400

    Motifications du script credits
    Blablabla meilleure quête ever

--- Assets/scripts/Scene_credits.cs
+++ Assets/scripts/Scene_credits.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Scene_credits : MonoBehaviour
 {
     public string credits;
     public AudioClip sonClic;
+    AudioSource audioSource;
 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void allerSurCredits()
     {
+        audioSource.PlayOneShot(sonClic, 1.2f);
+        Invoke("DelaiChargementScene", 1f);
+    }
+
+    void DelaiChargementScene()
+    {
         SceneManager.LoadScene(credits);
-        GetComponent<AudioSource>().PlayOneShot(sonClic);
     }
 }
