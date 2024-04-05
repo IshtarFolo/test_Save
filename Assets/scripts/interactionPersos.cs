fbd47285d49d037ef85e995ee4c7688cf9d225b4
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-04-05 18:29:46 -0400

    Une version de dialogue
    La touche E est enfoncée et un dialogue au milieu de l'écran est affiché

--- Assets/scripts/interactionPersos.cs
+++ Assets/scripts/interactionPersos.cs
@@ -8,31 +8,42 @@ public class interactionPersos : MonoBehaviour
     public TextMeshProUGUI lettreE;
     public bool veutParler = false;
 
-    public TextAsset texteJson;
+    public TextMeshProUGUI dialogueVilleagois; // Référence au texte du villageois actuel
+    private int currentDialogueIndex = 0; // Index du dialogue actuel
 
-    [System.Serializable]
-    public class dialogues
-    {
-        public int id;
-        public string contenu;
-    }
-
-    public GameObject dialogue;
+    public string[] dialogues; // Tableau de textes pour les dialogues
 
     // Start is called before the first frame update
     void Start()
     {
-        //dialogue = JsonUtility.FromJson<dialogues>(json);
+        dialogueVilleagois = dialogueVilleagois.GetComponent<TextMeshProUGUI>();
+        if (dialogues.Length > 0)
+            dialogueVilleagois.text = dialogues[currentDialogueIndex];
+            dialogueVilleagois.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (veutParler == true)
+        if (veutParler)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                lettreE.color = new Color(255, 255, 255);
+                // Afficher le prochain dialogue s'il y en a un
+                if (currentDialogueIndex < dialogues.Length - 1)
+                {
+                    currentDialogueIndex++;
+                    dialogueVilleagois.text = dialogues[currentDialogueIndex];
+                    dialogueVilleagois.enabled = true;
+                }
+                else
+                {
+                    // Cacher le texte s'il n'y a plus de dialogues
+                    dialogueVilleagois.text = "";
+                    veutParler = false;
+                    lettreE.enabled = false;
+                    dialogueVilleagois.enabled = false;
+                }
             }
         }
     }
@@ -48,5 +59,6 @@ public class interactionPersos : MonoBehaviour
     {
         lettreE.enabled = false;
         veutParler = false;
+        dialogueVilleagois.enabled = false;
     }
 }
