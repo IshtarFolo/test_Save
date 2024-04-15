0e022ec7a8aebcdffca1cd5983af086484909af1
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-04-15 15:33:36 -0400

    ctrl poncho correct + animations kirie

--- Assets/scripts/controlePoncho.cs
+++ Assets/scripts/controlePoncho.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 
 public class controlePoncho : MonoBehaviour
 {
-    public GameObject poncho;
-    public GameObject kirie;
-    float speed = 1.0f;
+    /*------------
+     * VARIABLES *
+     ------------*/
+    public GameObject poncho; // Reference au poncho
+    public GameObject kirie; // Reference a Kirie
+    float speed = 1.0f; // Vitesse de suivi du poncho (pas de delai)
+    float ponchoY; // La positionY du poncho par rapport a Kirie
 
-    Rigidbody rb;
-    Animator anim;
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // 
+        // le poncho suit kirie peu importe sa position
         poncho.transform.position = Vector3.Lerp(poncho.transform.position, kirie.transform.position, Time.deltaTime * speed);
 
-        // 
+        // si Kirie fait face a une direction ou une autre
         if (kirie.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("idleDroite"))
         {
-            poncho.transform.position = new Vector3(poncho.transform.position.x, 2.57f, kirie.transform.position.z - 0.4f);
+            ponchoY = 2.57f;
+            poncho.transform.position = new Vector3(poncho.transform.position.x, ponchoY, kirie.transform.position.z - 0.4f);
         }
         else
         {
-            poncho.transform.position = new Vector3(poncho.transform.position.x, 2.57f, kirie.transform.position.z - 0.2f);
+            poncho.transform.position = new Vector3(poncho.transform.position.x, ponchoY, kirie.transform.position.z - 0.2f);
+        }
+
+        // Si Kirie saute
+        if (kirie.GetComponent<Animator>().GetBool("saute") == true && kirie.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("idleDroite"))
+        {
+            ponchoY = kirie.transform.position.y + 2.52f;
+            poncho.transform.position = new Vector3(poncho.transform.position.x, ponchoY, kirie.transform.position.z - 0.4f);
+        }
+        else if (kirie.GetComponent<Animator>().GetBool("saute") == true && kirie.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("idleGauche"))
+        {
+            ponchoY = kirie.transform.position.y + 2.52f;
+            poncho.transform.position = new Vector3(poncho.transform.position.x, ponchoY, kirie.transform.position.z - 0.2f);
+        }
+        else
+        {
+            ponchoY = 2.57f;
         }
     }
 }
--- Assets/scripts/quetes.cs
+++ Assets/scripts/quetes.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class quetes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private string[] tableauQuetes;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+
     }
 }
