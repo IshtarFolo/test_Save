e6ec8c6abb54a09a8fa3620ebb39f823103627e3
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-04-29 16:15:46 -0400

    Dialogue Niveau 3 jouable + Changement design bouton
    - Il manquerai à avoir des conversations plausibles c.a.d continuer les discussions
    - Il faut aussi réarranger l'emplacement des dialogues. (celui de thays doit-être changer)

--- Assets/scripts/GestionDialogueThays.cs
+++ Assets/scripts/GestionDialogueThays.cs
@@ -31,25 +31,82 @@ public class GestionDialogueThays : MonoBehaviour
     public bool choixDiplomate = false;
     public bool choixColere = false;
 
-    private void Start()
+    [Header("l'index du dialogue actuelle")]
+    int ligneDialogueActuelle = 0;
+
+    float delaiEntreLettres = 0.10f;
+    bool ecrit = false;
+
+    void Start()
     {
-        
+        //bulleKirie.SetActive(false);
+        //bulleThays.SetActive(false);
+       
     }
 
-    private void Update()
+
+   IEnumerator CommencerConversation()
     {
-        
+        if (choixDiplomate)
+        {
+            // Afficher les dialogues diplomatiques
+            for (int i = 0; i < choixDiplomateKirie.Length; i++)
+            {
+                dialogueKirie.text = "";
+                dialogueThays.text = "";
+                ecrit = true;
+                //WaitUntil - Pour attendre que la variable devienne false pour continuer la conversation
+                StartCoroutine(AfficherDialogueParLettre(dialogueKirie, choixDiplomateKirie[i]));
+                yield return new WaitUntil(() => !ecrit);
+                yield return new WaitForSeconds(2f);
+                ecrit = true;
+                StartCoroutine(AfficherDialogueParLettre(dialogueThays, choixDiplomateThays[i]));
+                yield return new WaitUntil(() => !ecrit);
+                yield return new WaitForSeconds(2f);
+            }
+        }
+        else if (choixColere)
+        {
+            // Afficher les dialogues colériques
+            for (int i = 0; i < choixColereKirie.Length; i++)
+            {
+                dialogueKirie.text = "";
+                dialogueThays.text = "";
+                ecrit = true;
+                //WaitUntil - Pour attendre que la variable devienne false pour continuer la conversation
+                StartCoroutine(AfficherDialogueParLettre(dialogueKirie, choixColereKirie[i]));
+                yield return new WaitUntil(() => !ecrit);
+                yield return new WaitForSeconds(2f);
+                ecrit = true;
+                StartCoroutine(AfficherDialogueParLettre(dialogueThays, choixColereThays[i]));
+                yield return new WaitUntil(() => !ecrit);
+                yield return new WaitForSeconds(2f);
+            }
+        }
     }
 
     public void ChoixDiplomate()
     {
         choixDiplomate = true;
         lesOptions[1].SetActive(false);
+        StartCoroutine(CommencerConversation());
     }
 
     public void ChoixColere()
     {
         choixColere = true;
         lesOptions[0].SetActive(false);
+        StartCoroutine(CommencerConversation());
     }
+
+    IEnumerator AfficherDialogueParLettre(TextMeshProUGUI textMeshPro, string dialogue)
+    {
+        foreach (char letter in dialogue)
+        {
+            textMeshPro.text += letter;
+            yield return new WaitForSeconds(delaiEntreLettres);
+        }
+        ecrit = false;
+    }
+
 }
