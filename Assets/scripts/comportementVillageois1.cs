89a7e21e19fd4def4c514a095e96b74828d3ab40
Xavier Arbour <119866415+IshtarFolo@users.noreply.github.com>
2024-04-30 13:49:25 -0400

    ajout du villageois et programmation de son comportement

---
+++ Assets/scripts/comportementVillageois1.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class comportementVillageois1 : MonoBehaviour
+{
+    /*============
+     * VARIABLES *
+     ============*/
+    NavMeshAgent agent;
+    Animator animateur;
+
+    int indexDestinations = 0;
+    public Transform[] destinations;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animateur = GetComponent<Animator>();
+        
+        StartCoroutine(ChangerDestination());
+    }
+
+    void Update()
+    {
+        // On regarde si le NPC a atteint sa destination
+        if (!agent.pathPending && agent.remainingDistance < 0.5f && agent.enabled)
+        {
+            StartCoroutine(ChangerDestination());
+        }
+    }
+
+    IEnumerator ChangerDestination()
+    {
+        // Retourne if si il n'y a pas de destinations
+        if (destinations.Length == 0)
+            yield break;
+        // On envoie le NPC a la destination
+        agent.destination = destinations[indexDestinations].position;
+
+        // On choisi une nouvelle destination
+        indexDestinations = (indexDestinations + 1) % destinations.Length;
+
+        agent.isStopped = true;
+
+        // On attend un peu avant de relancer le mouvement du NPC
+        yield return new WaitForSeconds(10);
+
+        agent.isStopped = false;
+    }
+}
