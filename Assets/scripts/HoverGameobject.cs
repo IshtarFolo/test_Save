7b191c81d142330caad0c91ea05b531d60ea8a00
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-05-02 10:21:58 -0400

    Dialogue privée fonctionnel
    - Test avec des roches, simple mais parfait!

--- Assets/scripts/HoverGameobject.cs
+++ Assets/scripts/HoverGameobject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HoverGameobject : MonoBehaviour
 {
@@ -9,10 +10,24 @@ public class HoverGameobject : MonoBehaviour
     public Transform incrementElement; //The UI element I'm instantiating
     public Transform parentObj; //The UI Canvas
 
+    public TMPro.TMP_Text kiriePense;
+
     void OnMouseOver()
     {
+        if(gameObject.name == "Test_roche_1")
+        {
+            //Debug.Log(" Ceci est le test de la roche 1");
+            kiriePense.text = "Cette roche a l'air suspecte...";
+        }
+
+        if (gameObject.name == "Test_roche_2")
+        {
+            //Debug.Log(" Ceci est le test de la roche 2");
+            kiriePense.text = "Mais c'est une jolie roche ça!";
+        }
+
         //If your mouse hovers over the GameObject with the script attached, output this message
-        Debug.Log("Mouse is over GameObject.");
+        //Debug.Log("Mouse is over GameObject.");
 
         //mousePos = Camera.main.WorldToScreenPoint(Input.mousePosition); //Gets mouse position
     }
@@ -20,6 +35,7 @@ public class HoverGameobject : MonoBehaviour
     void OnMouseExit()
     {
         //The mouse is no longer hovering over the GameObject so output this message each frame
-        Debug.Log("Mouse is no longer on GameObject.");
+        //Debug.Log("Mouse is no longer on GameObject.");
+        kiriePense.text = "";
     }
 }
