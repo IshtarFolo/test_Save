c3dc6864b81aee6661edef4cf02e9763eaf85b1b
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-05-06 15:34:29 -0400

    Début de la gestion du son
    - Variable qui garde le son ajouté dans la scène des réglages

---
+++ Assets/scripts/GererMusique.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class GererMusique : MonoBehaviour
+{
+    public Slider mainSlider;
+    public float ValeurSon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Invoked when a submit button is clicked.
+    public void SubmitSliderSetting()
+    {
+        //Displays the value of the slider in the console.
+        Debug.Log(mainSlider.value);
+        ValeurSon = mainSlider.value;
+    }
+}
