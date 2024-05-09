e293831dd5f0e9ab9627fd895fa0babf98f4c793
Camilia El Moustarih <e0958030@cmaisonneuve.qc.ca>
2024-05-09 16:06:05 -0400

    Update MouvementPoissonUI.cs

--- Assets/scripts/MouvementPoissonUI.cs
+++ Assets/scripts/MouvementPoissonUI.cs
@@ -12,7 +12,7 @@ public class MouvementPoissonUI : MonoBehaviour
 
     //Gestion de la vitesse du poisson sur la barre UI
     public float vitessePoisson = 250f;
-    public float changerFrequence = 0.01f; //le poisson changera son mouvement à cette fréquence
+    public float changerFrequence = 0.005f; //le poisson changera son mouvement à cette fréquence
 
     //Gestion de la position du poisson
     public float poissonPosition;
