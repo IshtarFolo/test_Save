cf8865bfbeecb1ec15651b31f54f05e8542b768a
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-05-10 13:43:19 -0400

    Compteur de poissons mis en place !!!

--- Assets/scripts/SystemePeche.cs
+++ Assets/scripts/SystemePeche.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Script pour le système de pêche du niveau 2 de lac - Le joueur devra pêcher 3 poissons pour continuer la quête
+//Script pour le systï¿½me de pï¿½che du niveau 2 de lac - Le joueur devra pï¿½cher 3 poissons pour continuer la quï¿½te
 //Par Camilia El Moustarih
 
-//Liste d'énumération au cas où il y aurait plusieurs sources d'eau offrant différents poissons
-//Exemples océan, rivière, marécage, etc.
+//Liste d'ï¿½numï¿½ration au cas oï¿½ il y aurait plusieurs sources d'eau offrant diffï¿½rents poissons
+//Exemples ocï¿½an, riviï¿½re, marï¿½cage, etc.
 //Dans ce jeu, il n'y aura qu'un lac
 public enum SourceDeau
 {
@@ -24,7 +24,7 @@ public class SystemePeche : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
 
-        //S'assurer qu'il n'y a pas d'autre système de pêche dupliqué dans le jeu. Si oui, le supprimer.
+        //S'assurer qu'il n'y a pas d'autre systï¿½me de pï¿½che dupliquï¿½ dans le jeu. Si oui, le supprimer.
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -34,11 +34,11 @@ public class SystemePeche : MonoBehaviour
             Instance = this;
         }
 
-        //Le canvas du mini jeu est désactivé au début de la pêche, il sera réactivé seulement lorsqu'il y a contact avec un poisson autre que "Aucun poisson"
+        //Le canvas du mini jeu est dï¿½sactivï¿½ au dï¿½but de la pï¿½che, il sera rï¿½activï¿½ seulement lorsqu'il y a contact avec un poisson autre que "Aucun poisson"
         minijeu.SetActive(false);
     }
 
-    //Dans le cas où nous voudrions différentes sources d'eau qui offrent différents poissons
+    //Dans le cas oï¿½ nous voudrions diffï¿½rentes sources d'eau qui offrent diffï¿½rents poissons
     //Les poissons offerts ici sont
     //le saumon
     //la morue
@@ -46,7 +46,7 @@ public class SystemePeche : MonoBehaviour
     //le AucunPoisson
     public List<InfosPoissons> poissonsDeLac;
 
-    //Vérifier s'il y a contact et terminer la session de pêche
+    //Vï¿½rifier s'il y a contact et terminer la session de pï¿½che
     public bool siContactPoisson;
     private bool estEnTrainDeTirer;
 
@@ -62,18 +62,18 @@ public class SystemePeche : MonoBehaviour
 
     IEnumerator PecheCoroutine(SourceDeau sourceDeau)
     {
-        //Délai de 5 secondes avant contact avec poisson
+        //Dï¿½lai de 5 secondes avant contact avec poisson
         yield return new WaitForSeconds(5f);
 
-        //Récupérer la liste des poissons de cette source d'eau
+        //Rï¿½cupï¿½rer la liste des poissons de cette source d'eau
         InfosPoissons poisson = CalculerProbPoisson(sourceDeau);
 
         //S'il y a contact avec des poissons
-        //Si le poisson touché s'appelle AucunPoisson, le jeu s'arrête. Sinon, continuer avec les autres probabilités
+        //Si le poisson touchï¿½ s'appelle AucunPoisson, le jeu s'arrï¿½te. Sinon, continuer avec les autres probabilitï¿½s
         if (poisson.nomPoisson == "AucunPoisson")
         {
-            Debug.Log("Aucun contact, la session de pêche est terminée");
-            //La session de pêche se termine
+            Debug.Log("Aucun contact, la session de pï¿½che est terminï¿½e");
+            //La session de pï¿½che se termine
             PecheTerminee();
         }
         else
@@ -88,9 +88,9 @@ public class SystemePeche : MonoBehaviour
 
     internal void CommencerPeche(SourceDeau sourceDeau)
     {
-        //Looper à travers la liste de poissons et leurs probabilités de pêche
+        //Looper ï¿½ travers la liste de poissons et leurs probabilitï¿½s de pï¿½che
         StartCoroutine(PecheCoroutine(sourceDeau));
-        Debug.Log("peche commencée");
+        Debug.Log("peche commencï¿½e");
     }
 
     IEnumerator CommencerResistancePoisson(InfosPoissons poisson)
@@ -106,26 +106,26 @@ public class SystemePeche : MonoBehaviour
         Debug.LogWarning("Commencer mini-jeu");
         Debug.Log("Afficher maintenant le UI pour le miniJeu");
 
-        //Commencer le mini-jeu de pêche avec le ui
+        //Commencer le mini-jeu de pï¿½che avec le ui
         CommencerMiniJeuPeche();
     }
 
     internal void TerminerMiniJeu(bool reussite)
     {
-        //Désactiver le mini jeu et son canvas
+        //Dï¿½sactiver le mini jeu et son canvas
         minijeu.SetActive(false);
 
-        //Terminer la session de pêche
+        //Terminer la session de pï¿½che
         if (reussite)
         {
             PecheTerminee();
-            Debug.Log("poisson attrapé");
-            //Jouer le son de poisson capturé
+            Debug.Log("poisson attrapï¿½");
+            //Jouer le son de poisson capturï¿½
             audioSource.PlayOneShot(poissonCapture);
 
             
 
-            //Ajouter à l'inventaire
+            //Ajouter ï¿½ l'inventaire
 
             //CommencerMiniJeuPeche();
         }
@@ -133,23 +133,23 @@ public class SystemePeche : MonoBehaviour
         {
             PecheTerminee();
             Debug.Log("poisson perdu");
-            //Jouer le son de poisson capturé
+            //Jouer le son de poisson capturï¿½
             audioSource.PlayOneShot(poissonPerdu);
 
-            //Terminer la pêche
+            //Terminer la pï¿½che
             SystemePeche.Instance.PecheTerminee();
             estEnTrainDeTirer = false;
 
             //CommencerMiniJeuPeche();
 
-            //Recharger la scène en cas d'échec
-            SceneManager­.LoadScene("Niveau1_MiniJeuPeche");
+            //Recharger la scï¿½ne en cas d'ï¿½chec
+            SceneManager.LoadScene("Niveau1_MiniJeuPeche");
         }
     }
 
     private void CommencerMiniJeuPeche()
     {
-        //activer le mini jeu de pêche
+        //activer le mini jeu de pï¿½che
         minijeu.gameObject.SetActive(true);
 
     }
@@ -170,19 +170,19 @@ public class SystemePeche : MonoBehaviour
     {
         List<InfosPoissons> poissonDispo = CapturerPoissonDispo(sourceDeau);
 
-        //Calculer la probabilité
+        //Calculer la probabilitï¿½
         float probabiliteTotale = 0f;
         foreach (InfosPoissons poisson in poissonDispo)
-        // Truite = 5% (0-4) Aucun poisson = 10% (5-9) Saumon = 20% (10-19) Morue = 40% (20-39) = probabilité totale de : 75%
+        // Truite = 5% (0-4) Aucun poisson = 10% (5-9) Saumon = 20% (10-19) Morue = 40% (20-39) = probabilitï¿½ totale de : 75%
         {
             probabiliteTotale += poisson.probabiliteDattraper;
         }
 
-        //Générer un nombre aléatoire entre 0 et toutes les probabilités (75%)
+        //Gï¿½nï¿½rer un nombre alï¿½atoire entre 0 et toutes les probabilitï¿½s (75%)
         int nombreAleatoire = UnityEngine.Random.Range(0, Mathf.FloorToInt(probabiliteTotale) + 1); //entre 0 et 75
-        Debug.Log("Le nombre pigé est de " + nombreAleatoire);
+        Debug.Log("Le nombre pigï¿½ est de " + nombreAleatoire);
 
-        //Traverser la boucle des poissons et véfirier les probabilités
+        //Traverser la boucle des poissons et vï¿½firier les probabilitï¿½s
         float probabilitePigee = 0f;
         foreach (InfosPoissons poisson in poissonDispo)
         {
@@ -194,7 +194,7 @@ public class SystemePeche : MonoBehaviour
             }
         }
 
-        //En cas d'erreur de nombre aléatoire pigé
+        //En cas d'erreur de nombre alï¿½atoire pigï¿½
         return null;
     }
     public void PecheTerminee()
@@ -202,18 +202,18 @@ public class SystemePeche : MonoBehaviour
         siContactPoisson = false;
         estEnTrainDeTirer = false;
 
-        Debug.Log("Peche terminée");
+        Debug.Log("Peche terminï¿½e");
 
         
 
-        //Trigger pour terminer la manipulation de la canne à pêche
+        //Trigger pour terminer la manipulation de la canne ï¿½ pï¿½che
         //OnPecheTerminee?.Invoke();
 
         //CommencerPeche(SourceDeau.Lac);
     }
     private List<InfosPoissons> CapturerPoissonDispo(SourceDeau sourceDeau)
     {
-        //S'il y a plusieurs sources d'eau offrant différents poissons
+        //S'il y a plusieurs sources d'eau offrant diffï¿½rents poissons
         switch (sourceDeau)
         {
             case SourceDeau.Lac:
