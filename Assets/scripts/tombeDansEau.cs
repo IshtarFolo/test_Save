4cc0aa0bca1664f87b646ec8fbf5032c04fdf5c6
Xavier Arbour <arbour.xavier34@gmail.com>
2024-05-17 17:46:30 -0400

    ajout d'une animation de "noyade" et un script pour reload la scene apres

--- Assets/scripts/tombeDansEau.cs
+++ Assets/scripts/tombeDansEau.cs
@@ -7,11 +7,29 @@ public class tombeDansEau : MonoBehaviour
 {
     public GameObject manager;
 
+
+    Animator animateur;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        animateur = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnTriggerEnter(Collider infoTrigger)
     {
         if (infoTrigger.gameObject.tag == "lac")
         {
-            manager.GetComponent<savePosition>().Load();
+            animateur.SetTrigger("noyade");
+            rb.isKinematic = true;
+            StartCoroutine(Mort());
         }
+    }
+
+    IEnumerator Mort()
+    {
+        yield return new WaitForSeconds(2f);
+        manager.GetComponent<savePosition>().Load();
     }
 }
